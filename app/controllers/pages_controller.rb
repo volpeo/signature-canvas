820df84b42c4ee487628718ead16795c2aaa89c2
Sylvain Peigney <sylvain@volpeo.fr>
2018-08-30 18:20:34 +0200

    add signature

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+  end
+
+  def accept
+    @response = Cloudinary::Uploader.upload(params.dig(:signature, :content))
+
+    @image_id = @response['public_id']
   end
 end
